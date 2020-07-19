34b7ded31b0382709cacbf30462633e9b11039e4
Mohanraj <mthirumoorthy@manh.com>
2020-07-19 18:12:27 +0530

    Updates

---
+++ precache-manifest.c34636d28941bb0defb7ef8fdabd4fc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e6e9f2dde9ed6b6a9a5642128330f9",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d19cc645fa704e83abcc",
+    "url": "/app/static/css/main.592e5e29.chunk.css"
+  },
+  {
+    "revision": "f079be48558c98ca28a4",
+    "url": "/app/static/js/2.61912a47.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/app/static/js/2.61912a47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d19cc645fa704e83abcc",
+    "url": "/app/static/js/main.3fd91ae6.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/app/static/media/image.d7265326.png"
+  }
+]);
